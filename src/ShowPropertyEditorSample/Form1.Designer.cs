41211474017227911d74b7bf2c922c7dfdc34302
Reza Aghaei <rag@sitecore.net>
2017-12-02 03:26:06 +0800

    Changed folder structures and names.

--- src/ShowPropertyEditorSample/Form1.Designer.cs
+++ src/ShowPropertyEditorSample/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(260, 227);
+            this.ClientSize = new System.Drawing.Size(254, 226);
             this.Controls.Add(this.myControl1);
             this.Name = "Form1";
             this.Text = "Form1";
